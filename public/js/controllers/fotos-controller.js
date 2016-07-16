a60669576ac8df92e2c806ccd20fbb793ca39464
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2016-07-15 23:53:43 -0300

    5

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,8 @@
 angular.module('alurapic').controller('FotosController', function ($scope, $http) {
 	$scope.fotos = [];
 
+	$scope.filtro = '';
+
 	$http.get('v1/fotos')
 		.success(function (fotos) {
 			$scope.fotos = fotos;
@@ -10,13 +12,4 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 		});
 
 	return;
-
-	var promise = $http.get('v1/fotos');
-
-	promise.then(function (retorno) {
-		$scope.fotos = retorno.data;
-	})
-	.catch(function (error) {
-		console.log('error');
-	});
 });
---
+++ public/js/directives/minhas-directivas.js
@@ -0,0 +1,13 @@
+angular.module('minhasDiretivas', [])
+	.directive('meuPainel', function () {
+		var ddo = {};
+
+		ddo.restrict = 'AE';
+		ddo.scope = {
+			titulo: '@titulo'
+		};
+		ddo.transclude = true;
+		ddo.templateUrl = 'js/directives/meu-painel.html';
+
+		return ddo;
+	});
